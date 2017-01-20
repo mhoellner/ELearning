3645a079927f91bbeb00b36a23a558f7ccd2f6cc
Milan Höllner <milan.hoellner@gmail.com>
2017-01-20 22:12:41 +0100

    Adds improved forms

---
+++ ELearning/Templates.cs
@@ -0,0 +1,10 @@
+﻿namespace ELearning
+{
+  public static class Templates
+  {
+    public static int SingleChoiceQuestionTemplate = 1122;
+    public static int MultipleChoiceQuestionTemplate = 1126;
+    public static int RightAnswerTemplate = 1142;
+    public static int WrongAnswerTemplate = 1144;
+  }
+}
