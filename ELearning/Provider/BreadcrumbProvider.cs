14228829c13a62607f237048f6bd00808f2890ee
Milan Höllner <milan.hoellner@gmail.com>
2017-02-01 00:14:53 +0100

    Adding Breadcrumb (resolves #12)

---
+++ ELearning/Provider/BreadcrumbProvider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ELearning.Models;
+using Our.Umbraco.Ditto;
+using Umbraco.Web;
+
+namespace ELearning.Provider
+{
+    public class BreadcrumbProvider
+    {
+        public BreadcrumbProvider(int id)
+        {
+            UmbracoHelper helper = new UmbracoHelper(UmbracoContext.Current);
+            IEnumerable<string> itemIds = helper.TypedContent(id).Path.Split(',');
+            BreadcrumbItems = helper.TypedContent(itemIds).As<Item>().ToList();
+        }
+
+        public List<Item> BreadcrumbItems { get; private set; }
+    }
+}
