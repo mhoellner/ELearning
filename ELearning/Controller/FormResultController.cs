4a45a516d1ac24490c4908702539a2a9150f484e
Milan Höllner <milan.hoellner@gmail.com>
2017-02-06 19:38:59 +0100

    Correcting the calculation of all correct answers for a form (#3)

--- ELearning/Controller/FormResultController.cs
+++ ELearning/Controller/FormResultController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Web.Mvc;
 using ELearning.Models;
+using Our.Umbraco.Ditto;
 using Umbraco.Web.Models;
 
 namespace ELearning.Controller
@@ -12,30 +13,40 @@ namespace ELearning.Controller
         {
             int allCorrectAnswers = 0;
             int yourCorrectAnswers = 0;
-            List<string> questionIds = new List<string>();
 
             // POST-Arguments
-            var args = Request.Form.ToString().Split('&');
+            string formId = Request.Form["formId"];
+            string[] args = Request.Form.ToString().Split('&');
+
+            //Get amount of all correct answers for the current form
+            Form form = Umbraco.TypedContent(formId).As<Form>();
+            foreach (QuestionItem question in form.Questions)
+            {
+                allCorrectAnswers += question.Children.Count(c => c.DocumentTypeAlias == "correctAnswer");
+            }
 
             for (int i = 1; i < args.Length; i++)
             {
-                string[] ids = args[i].Split('=');
-                string questionId = ids[0].Substring(9);
-                string answerId = ids[1].Substring(7);
-
-                if (!questionIds.Contains(questionId))
-                {
-                    questionIds.Add(questionId);
-                    allCorrectAnswers += Umbraco.TypedContent(questionId).Children.Count(c => c.DocumentTypeAlias == "correctAnswer");
-                }
-                if (Umbraco.TypedContent(answerId).DocumentTypeAlias == "correctAnswer")
+                //Get amount of correct answers in the current request
+                string answerId = args[i].Split('=')[1];
+                string docAlias = Umbraco.TypedContent(answerId).DocumentTypeAlias;
+                if (docAlias == "correctAnswer")
                 {
                     yourCorrectAnswers++;
                 }
+                else if (docAlias == "wrongAnswer")
+                {
+                    yourCorrectAnswers--;
+                }
+            }
+            //Minimum for correct answers is 0
+            if (yourCorrectAnswers < 0)
+            {
+                yourCorrectAnswers = 0;
             }
             var formResult = new FormResult(Umbraco.AssignedContentItem)
             {
-                FormId = int.Parse(Request.Form["formId"]),
+                FormId = int.Parse(formId),
                 YourCorrectAnswers = yourCorrectAnswers,
                 AllCorrectAnswers = allCorrectAnswers,
                 FormTitle = "Auswertung"
