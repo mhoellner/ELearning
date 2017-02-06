e74903ff753a4e8df48d16089f45950188dfd8e7
Milan Höllner <milan.hoellner@gmail.com>
2017-02-06 17:58:12 +0100

    Adding Controller Action for Form Result. (resolves #3)

--- ELearning/Controller/FormResultController.cs
+++ ELearning/Controller/FormResultController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 using ELearning.Models;
 using Umbraco.Web.Models;
 
@@ -8,12 +10,34 @@ namespace ELearning.Controller
     {
         public override ActionResult Index(RenderModel model)
         {
-            var args = Request.Form;
-            var formResult = new FormResult(CurrentPage)
+            int allCorrectAnswers = 0;
+            int yourCorrectAnswers = 0;
+            List<string> questionIds = new List<string>();
+
+            // POST-Arguments
+            var args = Request.Form.ToString().Split('&');
+
+            for (int i = 1; i < args.Length; i++)
             {
-                FormId = 1095,
-                YourCorrectAnswers = 2,
-                AllCorrectAnswers = 3,
+                string[] ids = args[i].Split('=');
+                string questionId = ids[0].Substring(9);
+                string answerId = ids[1].Substring(7);
+
+                if (!questionIds.Contains(questionId))
+                {
+                    questionIds.Add(questionId);
+                    allCorrectAnswers += Umbraco.TypedContent(questionId).Children.Count(c => c.DocumentTypeAlias == "correctAnswer");
+                }
+                if (Umbraco.TypedContent(answerId).DocumentTypeAlias == "correctAnswer")
+                {
+                    yourCorrectAnswers++;
+                }
+            }
+            var formResult = new FormResult(Umbraco.AssignedContentItem)
+            {
+                FormId = int.Parse(Request.Form["formId"]),
+                YourCorrectAnswers = yourCorrectAnswers,
+                AllCorrectAnswers = allCorrectAnswers,
                 FormTitle = "Auswertung"
             };
             return View("~/Views/FormResult.cshtml", (object) formResult);
