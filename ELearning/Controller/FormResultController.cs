3a17d7edf5a2a7176e41a8d944505f127900b3f4
Milan Höllner <milan.hoellner@gmail.com>
2017-03-09 15:45:29 +0100

    Working on stats

--- ELearning/Controller/FormResultController.cs
+++ ELearning/Controller/FormResultController.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web.Mvc;
 using ELearning.Models;
@@ -17,7 +18,8 @@ namespace ELearning.Controller
         private int _yourCorrectAnswers;
         private string _formId;
         private FormResultSerializable _json;
-        private string _jsonPath = "/formresults/";
+        private string _jsonPath = "/formresults";
+        private Form _form;
 
         public override ActionResult Index(RenderModel model)
         {
@@ -27,14 +29,15 @@ namespace ELearning.Controller
             // POST-Arguments
             _formId = Request.Form["formId"];
             string[] args = Request.Form.ToString().Split('&');
+            
+            //Get amount of all correct answers for the current form
+            _form = Umbraco.TypedContent(_formId).As<Form>();
 
             //Trying to create a json file
-            InitializeJson();
-            SaveJson();
+            ReadJson();
+            _json.NumberOfResults++;
 
-            //Get amount of all correct answers for the current form
-            Form form = Umbraco.TypedContent(_formId).As<Form>();
-            foreach (QuestionItem question in form.Questions)
+            foreach (QuestionItem question in _form.Questions)
             {
                 _allCorrectAnswers += question.Children.Count(c => c.DocumentTypeAlias == "correctAnswer");
             }
@@ -46,6 +49,10 @@ namespace ELearning.Controller
             {
                 _yourCorrectAnswers = 0;
             }
+
+            _json.TestResults.Add(_yourCorrectAnswers);
+            
+            SaveJson();
 
             return View("~/Views/FormResult.cshtml", BuildModel());
         }
@@ -67,6 +74,18 @@ namespace ELearning.Controller
                 else if (docAlias == "wrongAnswer")
                 {
                     _yourCorrectAnswers--;
+                }
+                IncrementAnswerTimesClicked(answerId);
+            }
+        }
+
+        private void IncrementAnswerTimesClicked(string id)
+        {
+            foreach (QuestionSerializable question in _json.Questions)
+            {
+                if (question.Answers.Any(x => x.AnswerId == id))
+                {
+                    question.Answers.First(x => x.AnswerId == id).TimesClicked++;
                 }
             }
         }
@@ -92,7 +111,7 @@ namespace ELearning.Controller
         /// <returns>path</returns>
         private string PathWithFilename()
         {
-            return _jsonPath + _formId + ".json";
+            return _jsonPath + "/" + _formId + ".json";
         }
 
         /// <summary>
@@ -121,7 +140,11 @@ namespace ELearning.Controller
         /// <returns></returns>
         private bool JsonFileExists()
         {
-            return true;
+            if (Directory.Exists(_jsonPath) && System.IO.File.Exists(PathWithFilename()))
+            {
+                return true;
+            }
+            return false;
         }
 
         /// <summary>
@@ -129,6 +152,7 @@ namespace ELearning.Controller
         /// </summary>
         private void SaveJson()
         {
+            Directory.CreateDirectory(_jsonPath);
             using (StreamWriter writer = new StreamWriter(PathWithFilename()))
             {
                 writer.Write(JsonConvert.SerializeObject(_json, Formatting.Indented));
@@ -140,24 +164,48 @@ namespace ELearning.Controller
         /// </summary>
         private void InitializeJson()
         {
-            _json = new FormResultSerializable();
-            _json.FormId = _formId;
-            _json.NumberOfResults = 0;
-            _json.Questions = new[]
+            _json = new FormResultSerializable
             {
-                new QuestionSerializable
-                {
-                    QuestionId = "1000",
-                    Answers = new[]
-                    {
-                        new AnswerSerializable
-                        {
-                            AnswerId = "1001",
-                            AmountOfRightAnswers = 0
-                        }
-                    }
-                }
+                FormId = _formId,
+                NumberOfResults = 0,
+                TestResults = new List<int>(),
+                Questions = new QuestionSerializable[_form.Questions.Count()]
             };
+
+            int questionCounter = 0;
+            foreach (QuestionItem formQuestion in _form.Questions)
+            {
+                _json.Questions[questionCounter] = new QuestionSerializable
+                {
+                    QuestionId = formQuestion.Id.ToString(),
+                    Type = formQuestion.DocumentTypeAlias,
+                    Answers = new AnswerSerializable[formQuestion.Children.Count()]
+                };
+
+                int answerCounter = 0;
+                foreach (AbstractAnswer answer in formQuestion.Children.As<AbstractAnswer>())
+                {
+                    bool isCorrect = false;
+
+                    if (answer.DocumentTypeAlias == "correctAnswer")
+                    {
+                        isCorrect = true;
+                    }
+                    else if (answer.DocumentTypeAlias == "wrongAnswer")
+                    {
+                        isCorrect = false;
+                    }
+                    _json.Questions[questionCounter].Answers[answerCounter] = new AnswerSerializable
+                    {
+                        TimesClicked = 0,
+                        IsCorrect = isCorrect,
+                        AnswerId = answer.Id.ToString()
+                    };
+
+                    answerCounter++;
+                }
+                questionCounter++;
+            }
         }
     }
 }
--- ELearning/Models/FormResultSerializable.cs
+++ ELearning/Models/FormResultSerializable.cs
@@ -1,21 +1,26 @@
-﻿namespace ELearning.Models
+﻿using System.Collections.Generic;
+
+namespace ELearning.Models
 {
     public class FormResultSerializable
     {
         public string FormId { get; set; }
         public int NumberOfResults { get; set; }
+        public List<int> TestResults { get; set; }
         public QuestionSerializable[] Questions { get; set; }
     }
 
     public class QuestionSerializable
     {
         public string QuestionId { get; set; }
+        public string Type { get; set; }
         public AnswerSerializable[] Answers { get; set; }
     }
 
     public class AnswerSerializable
     {
         public string AnswerId { get; set; }
-        public int AmountOfRightAnswers { get; set; }
+        public int TimesClicked { get; set; }
+        public bool IsCorrect { get; set; }
     }
 }
