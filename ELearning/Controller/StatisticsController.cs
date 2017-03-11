ed16cb04dc6645c68cc9887ba10d4037bc971c6e
Milan Höllner <milan.hoellner@gmail.com>
2017-03-11 03:14:28 +0100

    First try of a new Controller with corresponding view

---
+++ ELearning/Controller/StatisticsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using ELearning.Models;
+using Our.Umbraco.Ditto;
+using Umbraco.Web.Models;
+
+namespace ELearning.Controller
+{
+    public class StatisticsController : Umbraco.Web.Mvc.RenderMvcController
+    {
+        [HttpGet]
+        public override ActionResult Index(RenderModel model)
+        {
+            Statistics stats = new Statistics(Umbraco.AssignedContentItem)
+            {
+                Title = (string) model.Content.GetProperty("title").Value,
+                FormsFolder = (int) model.Content.GetProperty("formsFolder").Value
+            };
+            return CurrentTemplate(stats);
+        }
+    }
+}
---
+++ ELearning/Models/Statistics.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using Umbraco.Core.Models;
+using Umbraco.Web.Models;
+
+namespace ELearning.Models
+{
+    public class Statistics : RenderModel
+    {
+        public Statistics(IPublishedContent content, CultureInfo culture) : base(content, culture)
+        {
+        }
+
+        public Statistics(IPublishedContent content) : base(content)
+        {
+        }
+
+        public int FormsFolder { get; set; }
+        public string Title { get; set; }
+    }
+}
